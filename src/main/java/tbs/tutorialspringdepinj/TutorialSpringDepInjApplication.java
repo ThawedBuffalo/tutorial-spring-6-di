7960cf31aaa17eec937c7f54335fde005b56da05
JC <thawedbuffalo@gmail.com>
2024-05-11 07:01:10 -0400

    init

---
+++ src/main/java/tbs/tutorialspringdepinj/TutorialSpringDepInjApplication.java
@@ -0,0 +1,13 @@
+package tbs.tutorialspringdepinj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringDepInjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialSpringDepInjApplication.class, args);
+    }
+
+}
